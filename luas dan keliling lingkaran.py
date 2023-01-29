15b52bc1bbff3288468ff2046c0142150fb8f78e
Trachine <123666514+Trachine@users.noreply.github.com>
2023-01-29 19:12:48 +0700

    Create luas dan keliling lingkaran.py

---
+++ luas dan keliling lingkaran.py
@@ -0,0 +1,9 @@
+# input nilai variable
+r = float(input("Masukan nilai jari-jari : "))
+phi = 3.14
+luas = phi*r*r
+keliling = 2*phi*r
+
+# menampilkan hasil dan membuat nilai keliling menjadi 2 angkat di belang koma
+print("Luas dari lingkaran adalah =",round(luas,2))
+print("Keliling dari lingkaran adalah =",round(keliling,2))
