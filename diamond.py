ed2b6cc83df7989d5dc943de3acbeef0268bebbc
Trachine <123666514+Trachine@users.noreply.github.com>
2023-01-29 19:22:53 +0700

    Create diamond.py

---
+++ diamond.py
@@ -0,0 +1,27 @@
+# menampilkan kalimat
+print("Hello ini script python")
+
+# menampilkan ganti baris
+print()
+
+#menampilkan karakter * dengan rata kanan diposisi 12
+print('{0:>12}'.format('*'))
+
+#menampilkan karakter * dengan rata kanan diposisi 12 lalu dipangkatkan 2 dan menampikan karakter terakhir * yang dipangkatkan lalu menampilkan garis ganti
+print('{0:>12}'.format('*'*2), end='*'*1)
+print()
+print('{0:>12}'.format('*'*3), end='*'*2)
+print()
+print('{0:>12}'.format('*'*4), end='*'*3)
+print()
+print('{0:>12}'.format('*'*5), end='*'*4)
+print()
+print('{0:>12}'.format('*'*4), end='*'*3)
+print()
+print('{0:>12}'.format('*'*3), end='*'*2)
+print()
+print('{0:>12}'.format('*'*2), end='*'*1)
+print()
+
+#menampilkan karakter * dengan rata kanan diposisi 12
+print('{0:>12}'.format('*'))
