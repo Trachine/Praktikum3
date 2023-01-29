19effc7b3c18ab8b6260546a1b485522c05f7b6a
Trachine <123666514+Trachine@users.noreply.github.com>
2023-01-29 19:14:09 +0700

    Create end.py

---
+++ end.py
@@ -0,0 +1,12 @@
+# menampilkan karakter dengan mengosongkan karakter terakhir(yang seharusnya ganti garis)
+print('A', end='')
+print('B', end='')
+print('C', end='')
+
+# menampilkan ganti baris
+print()
+
+# menampilkan karakter
+print('X')
+print('Y')
+print('Z')
